8f10cacd7a7d2fb99a04f4e2254178cd88e06bf9
Eric Baldwin <Ericbaldwinn@gmail.com>
2022-01-12 20:07:37 -0800

    All tables are populated with data correctly

--- schema.sql
+++ schema.sql
@@ -26,8 +26,8 @@ CREATE TABLE styles (
  id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
  productId INTEGER NOT NULL,
  name VARCHAR(50) NOT NULL,
- original_price INTEGER NOT NULL Check (original_price > 0),
- sale_price INTEGER CHECK (sale_price = null OR sale_price <= original_price),
+ original_price TEXT NOT NULL,
+ sale_price TEXT,
  default_style INTEGER NOT NULL
 );
 
@@ -36,8 +36,8 @@ CREATE TABLE styles (
 CREATE TABLE Skus (
  id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
  styleId INTEGER NOT NULL,
- quantity INTEGER NOT NULL,
- size VARCHAR(10) NOT NULL
+ size VARCHAR(10) NOT NULL,
+ quantity INTEGER NOT NULL
 );
 
 
@@ -58,8 +58,8 @@ CREATE TABLE Related (
 );
 
 
-ALTER TABLE styles ADD CONSTRAINT styles_productId_fkey FOREIGN KEY (productId) REFERENCES Product(id);
-ALTER TABLE Skus ADD CONSTRAINT Skus_styleId_fkey FOREIGN KEY (styleId) REFERENCES styles(id);
-ALTER TABLE Photos ADD CONSTRAINT Photos_styleId_fkey FOREIGN KEY (styleId) REFERENCES styles(id);
-ALTER TABLE Features ADD CONSTRAINT Features_product_id_fkey FOREIGN KEY (product_id) REFERENCES Product(id);
-ALTER TABLE Related ADD CONSTRAINT Related_current_product_id_fkey FOREIGN KEY (current_product_id) REFERENCES Product(id);
+-- ALTER TABLE styles ADD CONSTRAINT styles_productId_fkey FOREIGN KEY (productId) REFERENCES Product(id);
+-- ALTER TABLE Skus ADD CONSTRAINT Skus_styleId_fkey FOREIGN KEY (styleId) REFERENCES styles(id);
+-- ALTER TABLE Photos ADD CONSTRAINT Photos_styleId_fkey FOREIGN KEY (styleId) REFERENCES styles(id);
+-- ALTER TABLE Features ADD CONSTRAINT Features_product_id_fkey FOREIGN KEY (product_id) REFERENCES Product(id);
+-- ALTER TABLE Related ADD CONSTRAINT Related_current_product_id_fkey FOREIGN KEY (current_product_id) REFERENCES Product(id);
