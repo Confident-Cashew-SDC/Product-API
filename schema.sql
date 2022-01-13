3d21c24e9ed1304801728a862921cb9b04d5fe6a
Eric Baldwin <Ericbaldwinn@gmail.com>
2022-01-13 15:51:05 -0800

    Added Foreign Keys

--- schema.sql
+++ schema.sql
@@ -1,65 +1,62 @@
-DROP DATABASE productapi;
+DROP DATABASE IF EXISTS products;
 
-Create database productapi;
+CREATE DATABASE products;
 
-\c productapi
+\c products
 
-CREATE TABLE Product (
- id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
- name VARCHAR(50) NOT NULL,
- slogan TEXT,
- description TEXT,
- category VARCHAR(25) NOT NULL,
- default_price INTEGER NOT NULL
+CREATE TABLE product (
+  id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+  name VARCHAR(50) NOT NULL,
+  slogan TEXT,
+  description TEXT,
+  category VARCHAR(25) NOT NULL,
+  default_price INTEGER NOT NULL
 );
-
-
-CREATE TABLE Features (
- id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
- product_id INTEGER NOT NULL,
- feature VARCHAR(50) NOT NULL,
- value VARCHAR(50) NOT NULL
-);
-
 
 CREATE TABLE styles (
- id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
- productId INTEGER NOT NULL,
- name VARCHAR(50) NOT NULL,
- sale_price TEXT,
- original_price INTEGER NOT NULL,
- default_style INTEGER NOT NULL
+  id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+  product_id INTEGER NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  sale_price TEXT,
+  original_price INTEGER NOT NULL CHECK (original_price >= 0),
+  default_style BOOLEAN NOT NULL,
+  FOREIGN KEY(product_id)
+  REFERENCES product(id)
+);
+
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+  style_id INTEGER NOT NULL,
+  url TEXT,
+  thumbnail_url TEXT,
+  FOREIGN KEY(style_id)
+  REFERENCES styles(id)
+);
+
+CREATE TABLE skus (
+  id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+  style_id INTEGER NOT NULL,
+  size VARCHAR(10) NOT NULL,
+  quantity INTEGER NOT NULL,
+  FOREIGN KEY(style_id)
+  REFERENCES styles(id)
+);
+
+CREATE TABLE features (
+  id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+  product_id INTEGER NOT NULL,
+  feature VARCHAR(50) NOT NULL,
+  value TEXT,
+  FOREIGN KEY(product_id)
+  REFERENCES product(id)
+);
+
+CREATE TABLE related (
+  id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+  current_product_id INTEGER NOT NULL,
+  FOREIGN KEY(current_product_id)
+  REFERENCES product(id),
+  related_product_id INTEGER NOT NULL
 );
 
 
-
-CREATE TABLE Skus (
- id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
- styleId INTEGER NOT NULL,
- size VARCHAR(10) NOT NULL,
- quantity INTEGER NOT NULL
-);
-
-
-
-CREATE TABLE Photos (
- id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
- styleId INTEGER,
- url TEXT NOT NULL,
- thumbnail_url TEXT NOT NULL
-);
-
-
-
-CREATE TABLE Related (
- id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
- current_product_id INTEGER NOT NULL,
- related_product_id INTEGER NOT NULL
-);
-
-
--- ALTER TABLE styles ADD CONSTRAINT styles_productId_fkey FOREIGN KEY (productId) REFERENCES Product(id);
--- ALTER TABLE Skus ADD CONSTRAINT Skus_styleId_fkey FOREIGN KEY (styleId) REFERENCES styles(id);
--- ALTER TABLE Photos ADD CONSTRAINT Photos_styleId_fkey FOREIGN KEY (styleId) REFERENCES styles(id);
--- ALTER TABLE Features ADD CONSTRAINT Features_product_id_fkey FOREIGN KEY (product_id) REFERENCES Product(id);
--- ALTER TABLE Related ADD CONSTRAINT Related_current_product_id_fkey FOREIGN KEY (current_product_id) REFERENCES Product(id);
