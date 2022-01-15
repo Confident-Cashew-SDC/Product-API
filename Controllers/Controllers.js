7911d2a189866d9230301e5a5670aa3146261425
Eric Baldwin <Ericbaldwinn@gmail.com>
2022-01-15 15:11:08 -0800

    Implemented unoptimized JS logic to format product style data

--- Controllers/Controllers.js
+++ Controllers/Controllers.js
@@ -16,17 +16,48 @@ module.exports = {
         if (err) {
           res.status(404).send(err);
         } else {
-          res.send(results.rows[0])
+          const features = [];
+          results.rows.forEach((product) => {
+            features.push({feature: product.feature, value: product.value});
+          })
+          const final = results.rows[0];
+          delete final.feature;
+          delete final.value;
+          final.features = features;
+          res.send(final)
         }
       })
     }
   },
   getProductStyles: (req, res) => {
-    models.getProductStyles(req.query.product_id, (err, results) => {
+    models.getProductStyles(req.query.product_id, (err, response) => {
       if (err) {
         res.status(404).send(err)
       } else {
-        res.send(results.rows)
+        const final = {product_id: req.query.product_id, results: []};
+        for (var i = 0; i < response.rows.length; i++) {
+          if (final.results.length === 0 || final.results[final.results.length - 1].style_id !== response.rows[i].style_id) {
+            final.results.push({
+              style_id: response.rows[i].style_id,
+              name: response.rows[i].name,
+              original_price: response.rows[i].original_price,
+              sale_price: response.rows[i].sale_price,
+              "default?": response.rows[i].default_style,
+              photos: [{thumbnail_url: response.rows[i].thumbnail_url, url: response.rows[i].url}],
+              skus: {}
+            })
+            final.results[final.results.length - 1].skus[`${response.rows[i].sku}`] = { quantity: response.rows[i].quantity, size: response.rows[i].size};
+          } else {
+            if (final.results[final.results.length - 1].photos[final.results[final.results.length - 1].photos.length - 1].url !== response.rows[i].url) {
+              final.results[final.results.length - 1].photos.push({
+                thumbnail_url: response.rows[i].thumbnail_url,
+                url: response.rows[i].url
+              })
+            }
+            final.results[final.results.length - 1].skus[`${response.rows[i].sku}`] = { size: response.rows[i].size, quantity: response.rows[i].quantity};
+          }
+        }
+        res.send(final)
       }
     })
   }
--- Models/Models.js
+++ Models/Models.js
@@ -2,28 +2,23 @@ const db = require('../Database/database.js');
 
 module.exports = {
   getAllProducts: (callback) => {
-    // to do queries to product table
-    // return axios request
     var sqlString = ('Select * from product limit 5');
     db.query(sqlString, callback)
   },
   getProduct: (product_id, callback) => {
-    // need to join / inner join features to specific product id
-    var sqlString = (`Select * from product where id = ${product_id}`);
+    var sqlString = (`Select * from product Inner Join features ON features.product_id = product.id WHERE product.id = ${product_id}`);
     db.query(sqlString, callback)
   },
   getProductStyles: (product_id, callback) => {
-    var sqlString = (`Select * from styles where product_id = ${product_id}`);
+    console.log(product_id)
+    var sqlString = (`Select styles.id AS style_id, styles.product_id, styles.name, styles.sale_price, styles.original_price, styles.default_style, photos.thumbnail_url, photos.url, skus.id AS sku, skus.size, skus.quantity from styles
+    LEFT JOIN photos ON photos.style_id = styles.id
+    LEFT JOIN skus on skus.style_id = styles.id
+    WHERE styles.product_id = ${product_id}
+    ORDER BY styles.id`);
     db.query(sqlString, callback)
   }
 }
 
-// SELECT
-//     a,
-//     fruit_a,
-//     b,
-//     fruit_b
-// FROM
-//     basket_a
-// INNER JOIN basket_b
-//     ON fruit_a = fruit_b;
+
+
